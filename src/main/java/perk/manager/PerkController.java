25e42a39308fa4d24df42c7663ea63dcb87a09e7
Abduallah <AbduallahAldabibi@gmail.com>
2025-11-02 19:13:07 -0500

    Added post path for createPerk.

--- src/main/java/perk/manager/PerkController.java
+++ src/main/java/perk/manager/PerkController.java
@@ -66,7 +66,7 @@ public class PerkController {
         return "new-perk";
     }
 
-    @PostMapping
+    @PostMapping("/add")
     public String createPerk(
             @RequestParam String title,
             @RequestParam String description,

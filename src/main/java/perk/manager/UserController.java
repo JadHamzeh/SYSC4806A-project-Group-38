52742d76ba93d04cc44bc36ed46721b2b7193c7e
abeeh <adambeehler@cmail.carleton.ca>
2025-11-16 17:08:30 -0500

    Made an HTML fragment for login popup while on the dashboard. Added a GET-mapping to return the fragment of the form. Note: htmx.ajax call not working yet

--- src/main/java/perk/manager/UserController.java
+++ src/main/java/perk/manager/UserController.java
@@ -44,6 +44,11 @@ public class UserController {
         return "login";
     }
 
+    @GetMapping("/login-popup")
+    public String loginPopup() {
+        return "fragments/login-popup :: login-popup-form";
+    }
+
     /**
      * Displays the signup page.
      *

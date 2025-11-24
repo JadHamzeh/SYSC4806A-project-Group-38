ede6c575dffb8835ce6b7d472d6800a32bc418d7
JadHamzeh <jad.hamzeh8521@gmail.com>
2025-11-23 22:56:35 -0500

    added  comments to class

--- src/main/java/perk/manager/User.java
+++ src/main/java/perk/manager/User.java
@@ -1,38 +1,69 @@
 package perk.manager;
 
 import jakarta.persistence.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents an application user in the system.
+ *
+ * This entity is stored in the "users" table and maintains:
+ *  - Login credentials (username and password)
+ *  - Memberships the user belongs to
+ *  - Perks created by this user
+ */
 @Entity
 @Table(name = "users")
 public class User {
+
+    /**
+     * Primary key for the User entity.
+     * Uses auto-increment identity strategy.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    /** Username used for authentication. */
     private String username;
+
+    /** Password used for authentication (should be stored hashed). */
     private String password;
 
+    /**
+     * List of memberships the user is associated with.
+     *
+     * - mappedBy = "user": UserMembership has a 'user' field that owns the relationship.
+     * - CascadeType.ALL: membership rows will be updated/deleted when the user changes.
+     * - orphanRemoval = true: removing a membership from this list deletes it from the DB.
+     */
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<UserMembership> memberships = new ArrayList<>();
 
+    /**
+     * List of perks created by this user.
+     *
+     * This relationship is owned by the Perk entity (via its 'createdBy' field).
+     * No cascading is configured to avoid unintended deletion.
+     */
     @OneToMany(mappedBy = "createdBy")
     private List<Perk> perks = new ArrayList<>();
 
-    public User(){}
+    /** Default constructor required by JPA. */
+    public User() {}
 
+    /**
+     * Convenience constructor to create a user with credentials.
+     */
     public User(String username, String password){
         this.username = username;
         this.password = password;
     }
 
-    //getters setters
-    public Long getId() { return id; }
+    // Getters and setters
 
-    public void setId(long id) {
-        this.id = id;
-    }
+    public Long getId() { return id; }
+    public void setId(long id) { this.id = id; }
 
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
@@ -45,5 +76,4 @@ public class User {
 
     public List<Perk> getPerks() { return perks; }
     public void setPerks(List<Perk> perks) { this.perks = perks; }
-
 }
